085916e3fdf562ae238047ecef1a1d689f05fd9a
Ayan_khan_235 <babukhan640b@gmail.com>
2023-12-09 16:54:52 +0530

    ejs template

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app= express();
+const path=require("path");
 
 
 const port=8080;
 app.set("view engine", "ejs");
+// agr hum server ko bahar se start krte hai to ye ye views ko kabhi dhunch ni payega to isse bachnen ke liye hum path ka usekrte hai 
+app.set("views", path.join(__dirname,"/views"));
+
+
+app.get("/hello", (req,res)=>{
+    console.log("HELLO");
+});
+
 
 app.get("/", (req,res)=>{
     res.render("home.ejs");
-})
+});
+
+
 
 app.listen(port,()=>{
     console.log(`the server in listening on this port ${port}`);
-})
+});
